c6bec2356bbdc9b471fe521d6d9f591efb2cc576
Samuel Olsson <samuel.olsson@hitachigymnasiet.se>
2024-11-26 09:27:36 +0100

    Add files for models

---
+++ backend/queue_manager.py
@@ -0,0 +1,17 @@
+"""
+File for queue_manager. The class that should handle the queue of prints with functions to add, remove and get prints.
+"""
+
+class queue_manager():
+    def __init__(self):
+        self.prints = []
+
+    def add_new_print(self, print_to_add):
+        pass
+
+    def remove_print(self, print_to_remove):
+        pass
+
+    def get_next_print(self):
+        pass
+    
