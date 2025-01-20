2864affb83fd3b64768b63ad57aaa411aff5b9be
Samuel Olsson <samuel.olsson@hitachigymnasiet.se>
2025-01-20 13:57:29 +0100

    Fix not being able to print, switch lib for controlling printer, functions to read printer sdcards, currently needs two files to be modified to work with cloud support

--- backend/printer_manager.py
+++ backend/printer_manager.py
@@ -6,30 +6,82 @@ Description: Module with a class used to manage multiple printers (made for P1S 
 """
 
 
-from bambulabs_api import *
+# from bambulabs_api import *
+import time
+import sys
+
+import requests
+# from pathlib import Path
+# sys.path.append(str(Path(__file__).resolve().parent.parent))
+# from bambu_api_cloud.bambulabs_api import cloud2, client
+from bpm.bambuconfig import BambuConfig
+from bpm.bambuprinter import BambuPrinter
+from bpm.bambutools import PrinterState, PlateType
+from bpm.bambutools import parseStage
+from bpm.bambutools import parseFan
+
+import json
+
 from dotenv import load_dotenv
 import os
 
 class printer_manager():
-    def __init__(self):
+    def __init__(self, uid: str, access_token: str, region: str = "Europe"):
         load_dotenv()
 
         # Get environment variables for printers
-        self.NAMEs = os.getenv("Printer_NAMEs").split(",")
-        self.IPs = os.getenv("Printer_IPs").split(",")
-        self.SERIALs = os.getenv("Printer_SERIALs").split(",")
-        self.ACCESS_CODEs = os.getenv("Printer_ACCESS_CODEs").split(",")
+        self.uid = uid
+        self.cloud_username = f"u_{self.uid}"
+
+        self.access_token = access_token
+        self.region = region
 
         self.printers = {}
+        self.files = {}
 
-        # Add printers to the printer_managers "printers" dictionary, with its name as the key and the printer of class Printer as value
-        # Then connects to the printer
-        for name, ip, serial, access_code in zip(self.NAMEs, self.IPs, self.SERIALs, self.ACCESS_CODEs):
-            print(f"name: {name}\nip: {ip}\nserial: {serial}\naccess_code: {access_code}")
-            self.printers[name] = Printer(ip, access_code, serial)
-            self.printers[name].connect()
+        self.cloud_host = "https://api.bambulab.com"
+
+    def get_devices(self):
+        endpoint = "/v1/iot-service/api/user/bind"
+        # User agent needs to be specified to something, ohterwise bambulabs blocks it for some reason?!??!?!
+        headers = {"Authorization": f"Bearer {self.access_token}",
+                   "User-Agent": "curl/7.68.0"}
+        res = requests.get(f"{self.cloud_host}{endpoint}", headers=headers)
+
+        if not res.ok:
+            print(f"ERROR got response {res}")
+            raise Exception("Unable to get devices")
+        else:
+            return res.json()["devices"]
+
+
+    def connect_printers(self, printers_to_connect):
+
+        for printer in printers_to_connect:
+            print(printer)
+            try: 
+                printer_ip = os.getenv(printer["name"].replace(" ", "_"))
+            except KeyError:
+                print("Printer Name and IP not found in .env")
             
 
+            config = BambuConfig(ip=printer_ip, 
+                                 access_code=printer["dev_access_code"], 
+                                 mqtt_auth_token=self.access_token,
+                                 serial_number=printer["dev_id"],
+                                 mqtt_username_cloud=self.cloud_username)
+            printer_instance = BambuPrinter(config=config)
+
+            self.printers[printer["name"]] = printer_instance
+            try: 
+                self.printers[printer["name"]].start_session()
+            except Exception as e:
+                print("***WARNING***")
+                print("Failed to connect to printer")
+                print("***WARNING***")
+
+
+            
          
     def print_states(self, states_to_print:list[str] = None):
         """
@@ -51,7 +103,26 @@ class printer_manager():
 
         return states
     
-    def upload_print(self, printer_name:str, file_path:str):
+    def get_sdcard_files(self, printers: list | None = None, only_3mf_files: bool = True, get_from_printer = True):
+        if printers == None:
+            printers = list(self.printers.keys())
+        printer_files = {}
+        for printer in printers:
+            if not get_from_printer:
+                if not only_3mf_files:
+                    printer_files[printer] = self.printers[printer]._sdcard_contents
+
+                else:
+                    printer_files[printer] = self.printers[printer].get_sdcard_3mf_files()
+
+
+            else:    
+                printer_files[printer] = self.printers[printer].get_sdcard_contents()
+
+        self.files = printer_files
+        return printer_files    
+    
+    def upload_print(self, printer_name:str, local_file_path:str, printer_file_path):
         """
         Function to upload file to printer (.gcode or .3mf)
 
@@ -69,15 +140,10 @@ class printer_manager():
                     or
                     "No file uploaded." if an error occured during upload (will also raise exception)
         """
-        if file_path.split(".")[-1] != "gcode" and file_path.split(".")[-1] != "3mf":
-            return "Invalid file extension"
-
-        with open(file_path, "rb") as file:
-            file_name = file_path.split("/")[-1]
-            return self.printers[printer_name].upload_file(file, file_name)
+        return self.printers[printer_name].upload_sdcard_file(local_file_path, printer_file_path)
 
 
-    def start_print(self, printer_name:str, file_name:str):
+    def start_print_on_printer(self, printer_name:str, filename:str):
         """
         Function to start a print on a printer
         params:
@@ -90,7 +156,10 @@ class printer_manager():
 
         This function doesnt work, problem is believed to be that cloud connection is needed.
         """
-        return self.printers[printer_name].start_print(file_name, 0)
+        if filename.split(".")[-1] == "3mf":
+            return self.printers[printer_name].start_print(filename, 1, False)
+
+
 
     def get_printer_info(self, printer_name):
         pass
@@ -98,58 +167,39 @@ class printer_manager():
     def disconnect_printers(self, printer_names:list[str] = None):
         if printer_names == None:
             printer_names = list(self.printers.keys())
+            print(printer_names)
+
+        print(self.printers)
         for printer_name in printer_names:
-            self.printers[printer_name].disconnect()
+            self.printers[printer_name].quit()
 
 
 if __name__ == "__main__":
-    import time
-    p_man = printer_manager()
-    time.sleep(2)
 
-    # # Get the printer status
-    # states = p_man.print_states()
-    # print(list(p_man.printers.keys())[0])
-    printer_names = list(p_man.printers.keys())
-    # with open("prints/fan2.3mf", "rb") as file:
-    #     print(file.read())
-    # print("Uploading")
-    # p_man.upload_print(printer_names[0], "prints/fan4.gcode")
-    # print("Finished upload")
-    # time.sleep(2)
-    printer_1 = list(p_man.printers.keys())[0]
-    # print(p_man.printers[printer_1])
-    # print(p_man.printers[printer_1].PrintStatus.PRINTING)
+    load_dotenv()
 
-    # print(p_man.printers[printer_1].get_file_name())
-    # print("Trying to start print")
-    # p_man.printers[printer_1].start_print("fan3.gcode.3mf", 1)
-    # print("Command sent, hopefully")
+    UID = os.getenv("UID")
+    ACCESS_TOKEN = os.getenv("CLOUD_ACCESS_TOKEN")
+    REGION = os.getenv("REGION")
 
-    p_man.printers[printer_1].turn_light_on()
-    time.sleep(3)
-    frame = p_man.printers[printer_1].get_camera_frame()
-    with open("tests/image_frame.html", "w") as frame_file:
-        frame_txt = f"""
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Display Base64 Image</title>
-</head>
-<body>
-    <h1>Displaying Base64 Image</h1>
-    <img src="data:image/png;base64,{frame}" alt="Base64 Image" />
-</body>
-</html>
-"""
-        frame_file.write(frame_txt)
+    p_man = printer_manager(UID, ACCESS_TOKEN, REGION)
 
 
-    # print(f'Printer status: {states}')
-    # print("Starting print")
-    # print(p_man.start_print(list(p_man.printers.keys())[0], "fan4.gcode"))
-    # print("here")
-    # p_man.disconnect_printers()
+    devices = p_man.get_devices()
+    printers_to_connect_to = []
+    # Chooses a single printer
+    for printer in devices:
+        if printer["name"] == "S2. Daenerys Targaryen":
+            printers_to_connect_to.append(printer)
 
+    p_man.connect_printers(printers_to_connect_to)
+    # Need to sleep for a small amount of time after connect
+    time.sleep(1)
+
+    # Writes all printer files from all current printers to "printer_files.json"
+    files = p_man.get_sdcard_files()
+    with open("printer_files.json", "w") as file:
+        file.write(json.dumps(files, indent=4))
+
+
+    p_man.disconnect_printers()
